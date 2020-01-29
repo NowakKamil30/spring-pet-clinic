2fa9ec55ab41e7d745901da81eafcc491c8a1cab
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-29 02:07:56 +0100

    add id to models

--- pet-clinic-data/src/main/java/com/kamil/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/kamil/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.kamil.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/kamil/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/kamil/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.kamil.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
