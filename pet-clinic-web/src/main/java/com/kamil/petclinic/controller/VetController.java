a53b293ee46ed78e629cde9c55b270e064052ac9
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-30 15:57:17 +0100

    vet controller work

--- pet-clinic-web/src/main/java/com/kamil/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/kamil/petclinic/controller/VetController.java
@@ -1,9 +1,51 @@
 package com.kamil.petclinic.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kamil.petclinic.model.Owner;
+import com.kamil.petclinic.model.Vet;
+import com.kamil.petclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/vet")
 public class VetController {
+
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService =  vetService;
+    }
+
+    @GetMapping("")
+    public Set<Vet> getAllOwners()
+    {
+        return vetService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Vet getById(@PathVariable(value = "id") Long id)
+    {
+        return vetService.findById(id);
+    }
+
+    @PostMapping("")
+    public Vet postOwner(@RequestBody Vet vet)
+    {
+        return vetService.save(vet);
+    }
+
+    @PutMapping("/{id}")
+    public Vet putOwner(@PathVariable(value="id") Long id, @RequestBody Vet vet)
+    {
+        return vetService.change(id, vet);
+    }
+
+    @DeleteMapping("/{id}")
+    public Vet deleteById(@PathVariable(value="id") Long id)
+    {
+        return vetService.deleteById(id);
+    }
 }
