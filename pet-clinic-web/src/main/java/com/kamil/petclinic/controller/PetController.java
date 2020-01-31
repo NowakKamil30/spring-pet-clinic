5f23a77bed3be6259ac2f8ba4b0c5e0a0483d135
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-31 16:56:53 +0100

    updated PetController

--- pet-clinic-web/src/main/java/com/kamil/petclinic/controller/PetController.java
+++ pet-clinic-web/src/main/java/com/kamil/petclinic/controller/PetController.java
@@ -1,9 +1,41 @@
 package com.kamil.petclinic.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kamil.petclinic.model.Pet;
+import com.kamil.petclinic.services.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/pet")
 public class PetController {
+
+    @Autowired
+    private PetService petService;
+
+    @GetMapping("")
+    public Set<Pet> getAllPet(){
+        return petService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Pet getPetById(@PathVariable(value = "id") Long id){
+        return petService.findById(id);
+    }
+
+    @PostMapping("")
+    public Pet postPet(@RequestBody Pet pet){
+        return petService.save(pet);
+    }
+
+    @PutMapping("/{id}")
+    public Pet putPet(@PathVariable(value = "id")Long id,@RequestBody Pet pet){
+        return petService.change(id,pet);
+    }
+
+    @DeleteMapping("/{id}")
+    public Pet deletePetById(@PathVariable(value = "id")Long id){
+        return petService.deleteById(id);
+    }
 }
