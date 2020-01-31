66972538ebdef5f6deb74b31f4e93089818ef571
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-31 15:52:51 +0100

    updated Owner

--- pet-clinic-data/src/main/java/com/kamil/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/kamil/petclinic/model/Owner.java
@@ -5,6 +5,33 @@ import java.util.Set;
 public class Owner extends Person {
 
     private Set<Pet> pets;
+    private String address;
+    private String city;
+    private String telephone;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
