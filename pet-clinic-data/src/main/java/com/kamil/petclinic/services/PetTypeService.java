0e08689eada3f603d96a2274f6e555a1caef4f5e
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-31 16:00:30 +0100

    creted PetTypeService and PetTypeServiceMap

---
+++ pet-clinic-data/src/main/java/com/kamil/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.kamil.petclinic.services;
+
+import com.kamil.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/kamil/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,38 @@
+package com.kamil.petclinic.services.map;
+
+import com.kamil.petclinic.model.PetType;
+import com.kamil.petclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeServiceMap extends  AbstractMapService<PetType,Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType change(Long id, PetType obj) {
+        return super.change(id,obj);
+    }
+
+    @Override
+    public PetType delete(PetType obj) {
+        return super.delete(obj);
+    }
+
+    @Override
+    public PetType deleteById(Long id) {
+        return super.deleteById(id);
+    }
+
+    @Override
+    public PetType save(PetType obj) {
+        return super.save(obj);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
