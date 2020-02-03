f49fdcc44ea7af9eadd24498ecadc7a6695a8407
Kamil Nowak <kamilnowakx98@gmail.com>
2020-02-03 12:49:22 +0100

    added VisitService and VisitServiceMap

---
+++ pet-clinic-data/src/main/java/com/kamil/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.kamil.petclinic.services;
+
+import com.kamil.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/kamil/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,43 @@
+package com.kamil.petclinic.services.map;
+
+import com.kamil.petclinic.model.Visit;
+import com.kamil.petclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@Profile(value = "map")
+public class VisitServiceMap extends AbstractMapService<Visit,Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit change(Long id, Visit obj) {
+        return super.change(id, obj);
+    }
+
+    @Override
+    public void delete(Visit obj) {
+        super.delete(obj);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Visit save(Visit obj) {
+        return super.save(obj);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
